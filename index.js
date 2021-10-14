abff7ae1241829ea07954ec04eb14d44923ec097
Bridgette <sanderson.bridgette@gmail.com>
2021-10-14 15:20:07 -0500

    Modal Almost Complete

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/** 
-  Creating interactive buttons: Open Modal
-  
-  0) Add 'display: none' to '#overlay' - we want to hide it initially!
-  1) Use document.getElementById to target "open-modal"
-  2) Add an event listener of "click"
-  3) Use document.getElementById to target "overlay" and change the style.display to "block"
-**/
-
-/** 
-  Creating interactive buttons: Close Modal
-  
-  1) Use document.getElementById to target "close-modal"
-  2) Add an event listener of "click"
-  3) Use document.getElementById to target "overlay" and change the style.display to "none"
-**/
 
 
 // Open Modal------ 
