514da3409cba643a2fd85ebda42b70ecbaf8e331
Bridgette <sanderson.bridgette@gmail.com>
2021-10-14 14:57:13 -0500

    nvr shown attempt no googling

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+/** 
+  Creating interactive buttons: Open Modal
+  
+  0) Add 'display: none' to '#overlay' - we want to hide it initially!
+  1) Use document.getElementById to target "open-modal"
+  2) Add an event listener of "click"
+  3) Use document.getElementById to target "overlay" and change the style.display to "block"
+**/
+
+/** 
+  Creating interactive buttons: Close Modal
+  
+  1) Use document.getElementById to target "close-modal"
+  2) Add an event listener of "click"
+  3) Use document.getElementById to target "overlay" and change the style.display to "none"
+**/
+const openBtn = document.getElementById("open-modal")
+const closeBtn = document.getElementById("close-modal")
+const overlay = document.getElementById("overlay")
+
+
+openBtn.addEventListener("click", function() {
+    overlay.createElement 
+})
+
+closeBtn.addEventListener("click", function() {
+    console.log("warning will robinson")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // we need an event listener just attaches to an element (like a light switch) - in this case a DOM element, our button(s)!
 // DOM elements = anything you see on the page on the document (divs, buttons, tables, links--any HTML element!divs, buttons, tables, links--any HTML element!)
 // you can switch out "click" or be other types of DOM events (keydown, mouseover), any sort of event that a user can do, you can capture user actions using event listeners!
