d05a094150d0045d0bcca5a4aacfd472a975cfc5
Bridgette <sanderson.bridgette@gmail.com>
2021-10-15 10:06:15 -0500

    My attempt

--- searchbar-challenge/index.js
+++ searchbar-challenge/index.js
@@ -9,3 +9,25 @@ document.getElementById("searchInput").addEventListener("keyup", function(event)
     let searchQuery = event.target.value.toLowerCase();
     console.log(searchQuery)
 });
+
+/*
+    Task: Get all the name elements from the DOM
+    1. Create a new 'let' called 'allNamesDOMCollection'
+    2. Store all the elements of class name 'name' in 'allNamesDOMCollection'
+    3. Log to the console 'allNamesDOMCollection' to demonstrate that an HTMLCollection exists
+    4. Log out the first index of allNamesDOMCollection and add 'textContent' to view a name
+*/
+
+// My notes -- there is another method/element called .getElementsByClassName that the instructor mentioned.
+
+let allNamesDOMCollection = document.getElementsByClassName("name")
+
+
+for (let i = 0; i < allNamesDOMCollection.length; i++) {
+    let name = allNamesDOMCollection[i]
+    console.log(name)
+}
+
+
+console.log(allNamesDOMCollection)
+console.log(name)
