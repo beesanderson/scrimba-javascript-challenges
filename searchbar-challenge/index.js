943d61692a45f61f0a14a60560d643b964a936c8
Bridgette <sanderson.bridgette@gmail.com>
2021-10-15 10:55:34 -0500

    wip

--- searchbar-challenge/index.js
+++ searchbar-challenge/index.js
@@ -7,8 +7,11 @@
 
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase();
-    console.log(searchQuery)
+    let allNamesDOMCollection = document.getElementsByClassName("name")
+   console.log(allNamesDOMCollection[1].textContent) //so this will make all the names an array and based on the index [0-7] will list them out accordingly no matter what is typed
 });
+
+
 
 /*
     Task: Get all the name elements from the DOM
@@ -20,14 +23,16 @@ document.getElementById("searchInput").addEventListener("keyup", function(event)
 
 // My notes -- there is another method/element called .getElementsByClassName that the instructor mentioned.
 
-let allNamesDOMCollection = document.getElementsByClassName("name")
+//MY SAD SORRY ATTEMPT =>
+
+// let allNamesDOMCollection = document.getElementsByClassName("name")
 
 
-for (let i = 0; i < allNamesDOMCollection.length; i++) {
-    let name = allNamesDOMCollection[i]
-    console.log(name)
-}
+// for (let i = 0; i < allNamesDOMCollection.length; i++) {
+//     let allNames = allNamesDOMCollection[i];
+//     console.log(allNames)
+// }
 
 
-console.log(allNamesDOMCollection)
-console.log(name)
+// console.log(allNamesDOMCollection)
+// console.log(allNames)
